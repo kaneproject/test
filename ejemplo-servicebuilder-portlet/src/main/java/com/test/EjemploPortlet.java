dfc3777766e544e59cae0034bb2bdfa7448eca09
rgomez <rgomez@pronoide.es>
2016-03-29 21:39:57 +0200

    Agregado doview

--- ejemplo-servicebuilder-portlet/src/main/java/com/test/EjemploPortlet.java
+++ ejemplo-servicebuilder-portlet/src/main/java/com/test/EjemploPortlet.java
@@ -1,11 +1,25 @@
 package com.test;
 
+import java.io.IOException;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
 /**
  * Portlet implementation class EjemploPortlet
  */
 public class EjemploPortlet extends MVCPortlet {
+
+	@Override
+	public void doView(RenderRequest renderRequest,
+			RenderResponse renderResponse) throws IOException, PortletException {
+		// TODO Auto-generated method stub
+		super.doView(renderRequest, renderResponse);
+		System.out.println("Morcillas!");
+	}
  
 
 }
